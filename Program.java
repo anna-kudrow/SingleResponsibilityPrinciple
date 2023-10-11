7bd9e8d2915abe3608eea7628e33aa50efd3769e
Anna <anka-kudrow@mail.ru>
2023-10-11 22:32:14 +0300

    Have done homework

---
+++ Program.java
@@ -0,0 +1,17 @@
+package ru.GB.Lesson6.srp2;
+
+public class Program {
+    public static void main(String[] args) {
+
+        System.out.println("Input your order: ");
+
+        GetInformationAboutNewOrder infoOrder = new GetInformationAboutNewOrder();
+
+        Order order = new Order(infoOrder.getClientName(), infoOrder.getProduct(), infoOrder.getQnt(), infoOrder.getPrice());
+
+        SaveToJson saveToJson = new SaveToJson(order);
+
+        saveToJson.saveToJson();
+
+    }
+}
